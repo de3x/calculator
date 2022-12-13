d02ae642cdccd0c2075d551e216bedebe5b9a0ca
David Kelly <david.kelly1122@gmail.com>
2022-12-13 01:00:22 -0800

    Completely rebuild calculator to allow for two displays

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-let displayValue = 0;
-let number1, number2, selectedOperator;
-let num1array = [];
-let num2array = [];
-let answer;
-
-const display = document.getElementById("display");
+const topDisplay = document.querySelector(".top-display");
+const bottomDisplay = document.querySelector(".bottom-display");
 const numberButtons = document.querySelectorAll(".operand");
 const operatorButtons = document.querySelectorAll(".operator");
 const addButton = document.getElementById("add");
@@ -13,153 +8,393 @@ const multiplyButton = document.getElementById("multiply");
 const divideButton = document.getElementById("divide");
 const equalsButton = document.getElementById("equals");
 const resetButton = document.getElementById("reset");
+const signButton = document.getElementById("sign");
+const percentButton = document.getElementById("percent");
+const decimalButton = document.getElementById("decimal");
+const allButtons = document.querySelectorAll("button");
 
-// Update display value
-function updateDisplay() {
-  display.innerText = displayValue;
+let digitsArray = [];
+let currentNumber = 0;
+let currentTotal = undefined;
+let selectedOperator = undefined;
+let equalsPressed = false;
+let answer = undefined;
+let numberBtnPressed = false;
+let decimalJustPressed = false;
+// not sure if this is needed yet
+// let previousNumber;
+
+function setCurrentNumber() {
+  currentNumber = +digitsArray.join("");
+  updateBottomDisplay();
 }
 
-function updateFirstNumber() {
-  number1 = +num1array.join("");
-  displayValue = number1;
-  updateDisplay();
-  // Empty num1 array for future calculations
+function setCurrentTotal() {}
+
+function add(num1, num2) {
+  let answer = +(num1 + num2);
+  return answer;
+
+  //   currentTotal += currentNumber;
+  //   updateTopDisplay(`${currentTotal} +`);
+  //   updateBottomDisplay(currentNumber);
+  // todo
 }
 
-function updateSecondNumber() {
-  number2 = +num2array.join("");
-  displayValue = number2;
-  updateDisplay();
-  // Empty num1 array for future calculations
+function subtract(num1, num2) {
+  let answer = +(num1 - num2);
+  return answer;
 }
 
-// Give display initial placeholder value (0)
-updateDisplay();
+function multiply(num1, num2) {
+  let answer = +(num1 * num2);
+  return answer;
+}
 
-// TODO: add some keyboard functionality
-window.addEventListener("keydown", function (e) {
-  // Check if key is a number before storing in array
-  if (!isNaN(parseInt(e.key))) {
-    if (num1array.length < 9) {
-      num1array.push(e.key);
-      console.log(num1array);
-    }
+function divide(num1, num2) {
+  if ((num1 != 0) & (num2 === 0)) {
+    console.log(currentNumber);
+
+    // disable all buttons
+    allButtons.forEach((button) => {
+      button.disabled = true;
+    });
+
+    decimalButton.disabled = true;
+
+    // reenable "reset" button
+    resetButton.disabled = false;
+    resetButton.style.border = "2px solid yellow";
+    resetButton.style.scale = "1.075";
+    return "#DIV/0!";
   }
-  // Convert num1array to number1 whenever operator is pressed
-  //   console.log(e.key);
-});
+  let answer = +(num1 / num2);
+  return answer;
+}
 
-operatorButtons.forEach((button) => {
-  button.addEventListener("click", function (e) {
-    e.stopPropagation();
-    console.log(e);
+function reset() {
+  digitsArray = [];
+  currentNumber = undefined;
+  currentTotal = undefined;
+  answer = undefined;
+  selectedOperator = undefined;
+  equalsPressed = false;
+  numberBtnPressed = false;
+  decimalButton.disabled = false;
+  decimalJustPressed = false;
+
+  // reenable all buttons from when dividing by 0
+  allButtons.forEach((button) => {
+    button.disabled = false;
   });
-});
+
+  resetButton.style.border = "inherit";
+  resetButton.style.scale = "inherit";
+
+  clearBothDisplays();
+}
+
+function getOperatorSymbol() {
+  if (selectedOperator === "add") {
+    return "+";
+  } else if (selectedOperator === "subtract") {
+    return "-";
+  } else if (selectedOperator === "multiply") {
+    return "*";
+  } else if (selectedOperator === "divide") {
+    return "/";
+  } else if (selectedOperator === undefined) {
+    return "";
+  }
+}
+
+function updateTopDisplay(str) {
+  topDisplay.textContent = str;
+  //   let operatorSymbol = getOperatorSymbol();
+  //   if (selectedOperator === undefined) {
+  //     topDisplay.textContent = " ";
+  //   }
+  //   if ((currentNumber != undefined) & (selectedOperator != undefined)) {
+  //     topDisplay.textContent = `${currentTotal} ${operatorSymbol}`;
+  //   }
+  //   if ((equalsPressed = true)) {
+  //     topDisplay.textContent = `${currentTotal} ${operatorSymbol} ${currentNumber} =`;
+  //   }
+}
+
+function updateBottomDisplay(num) {
+  bottomDisplay.textContent = num;
+}
+
+function updateBothDisplays(topstring, bottomstring) {
+  updateTopDisplay(topstring);
+  updateBottomDisplay(bottomstring);
+}
+
+function clearTopDisplay() {
+  topDisplay.textContent = "";
+}
+
+function clearBottomDisplay() {
+  bottomDisplay.textContent = 0;
+}
+
+function clearBothDisplays() {
+  clearTopDisplay();
+  clearBottomDisplay();
+}
+
+function backspace() {
+  // todo : need to create element and link to it
+  // todo
+}
+
+function appendEqualsSign() {
+  if (!equalsPressed) {
+    topDisplay.textContent += " =";
+  }
+}
+
+function operate() {
+  appendEqualsSign();
+  equalsPressed = true;
+}
 
 numberButtons.forEach((button) => {
   button.addEventListener("click", function (e) {
     e.stopPropagation();
-    // Check if num1array empty to determine which number to update (1st or 2nd)
-    if (selectedOperator === undefined) {
-      if (num1array.length < 9) {
-        num1array.push(parseInt(button.textContent));
-        updateFirstNumber();
-      }
-    } else {
-      if (num2array.length < 9) {
-        num2array.push(parseInt(button.textContent));
-        updateSecondNumber();
+    numberBtnPressed = true;
+    // Reset calculator if equal button been pressed
+    if (equalsPressed === true) {
+      reset();
+      updateBothDisplays();
+    }
+
+    if (digitsArray.length < 13) {
+      // working way (not sure anymore about that)
+      //   digitsArray.push(parseInt(button.textContent));
+      //   currentNumber = +digitsArray.join("");
+      // testing way
+      if (button.disabled === false) {
+        digitsArray.push(button.textContent.trim());
+        let daj = digitsArray.join("").replace(/\s/g, "");
+        currentNumber = +daj;
+        console.log(currentNumber);
+        // handle decimal input
+        console.log(digitsArray);
+        updateBottomDisplay(`${currentNumber}`);
+
+        // handling decimals
+        if (button.textContent === ".") {
+          decimalButton.disabled = true;
+          if (digitsArray.length === 1) {
+            currentNumber = 0;
+            updateBottomDisplay(`${currentNumber}.`);
+          }
+          if (digitsArray.length > 1) {
+            // todo
+            updateBottomDisplay(`${currentNumber}.`);
+          }
+        }
       }
     }
-    console.log(parseInt(button.textContent));
   });
 });
 
-// Operator buttons functionality
 addButton.addEventListener("click", function (e) {
+  e.stopPropagation();
   selectedOperator = "add";
-  // Empty array for future calculations
-  num1array = [];
+
+  // handle changing display of active selectedOperator
+  if (topDisplay.textContent != "") {
+    updateTopDisplay(`${currentTotal} +`);
+  }
+
+  if (equalsPressed === true) {
+    // todo
+    currentTotal = +bottomDisplay.textContent;
+    updateTopDisplay(`${currentTotal} +`);
+    updateBottomDisplay("");
+    equalsPressed = false;
+  }
+
+  if (numberBtnPressed) {
+    //   if (3 > 1) {
+
+    selectedOperator = "add";
+    if (currentTotal === undefined) currentTotal = 0;
+    currentTotal = add(currentTotal, currentNumber);
+    updateTopDisplay(`${currentTotal} +`);
+    updateBottomDisplay("");
+
+    // updateBottomDisplay("");
+    // Empty array for next number
+    digitsArray = [];
+    numberBtnPressed = false;
+    decimalButton.disabled = false;
+  }
+  decimalButton.disabled = false;
 });
 
 subtractButton.addEventListener("click", function (e) {
-  // Convert array into number and store it and then display it
+  e.stopPropagation();
   selectedOperator = "subtract";
-  //   updateFirstNumber();
-  // Empty array for future calculations
-  num1array = [];
+
+  // handle changing display of active selectedOperator
+  if (topDisplay.textContent != "") {
+    updateTopDisplay(`${currentTotal} -`);
+  }
+
+  if (equalsPressed === true) {
+    currentTotal = +bottomDisplay.textContent;
+    updateBothDisplays(`${currentTotal} -`, "");
+    equalsPressed = false;
+  }
+
+  if (numberBtnPressed) {
+    // selectedOperator = "subtract";
+
+    if (currentTotal != undefined) {
+      currentTotal = subtract(currentTotal, currentNumber);
+      updateTopDisplay(`${currentTotal} -`);
+      updateBottomDisplay("");
+    }
+
+    if (currentTotal === undefined) {
+      currentTotal = currentNumber;
+      updateTopDisplay(`${currentTotal} -`);
+      updateBottomDisplay("");
+    }
+    // Empty array for next number
+    digitsArray = [];
+    numberBtnPressed = false;
+    decimalButton.disabled = false;
+  }
+  decimalButton.disabled = false;
 });
 
 multiplyButton.addEventListener("click", function (e) {
-  // Convert array into number and store it and then display it
+  e.stopPropagation();
   selectedOperator = "multiply";
-  //   updateFirstNumber();
-  // Empty array for future calculations
-  num1array = [];
+
+  // handle changing display of active selectedOperator
+  if (topDisplay.textContent != "") {
+    updateTopDisplay(`${currentTotal} *`);
+  }
+
+  if (equalsPressed) {
+    currentTotal = +bottomDisplay.textContent;
+    updateBothDisplays(`${currentTotal} *`, "");
+    equalsPressed = false;
+  }
+
+  if (numberBtnPressed) {
+    // selectedOperator = "subtract";
+
+    if (currentTotal != undefined) {
+      currentTotal = multiply(currentTotal, currentNumber);
+      updateTopDisplay(`${currentTotal} *`);
+      updateBottomDisplay("");
+    }
+
+    if (currentTotal === undefined) {
+      currentTotal = currentNumber;
+      updateTopDisplay(`${currentTotal} *`);
+      updateBottomDisplay("");
+    }
+    // Empty array for next number
+    digitsArray = [];
+    numberBtnPressed = false;
+    decimalButton.disabled = false;
+  }
+  decimalButton.disabled = false;
 });
 
 divideButton.addEventListener("click", function (e) {
-  // Convert array into number and store it and then display it
+  e.stopPropagation();
   selectedOperator = "divide";
-  //   updateFirstNumber();
-  // Empty array for future calculations
-  num1array = [];
+
+  // handle changing display of active selectedOperator
+  if (topDisplay.textContent != "") {
+    updateTopDisplay(`${currentTotal} /`);
+  }
+
+  if (equalsPressed) {
+    currentTotal = +bottomDisplay.textContent;
+    updateBothDisplays(`${currentTotal} /`, "");
+    equalsPressed = false;
+  }
+
+  if (numberBtnPressed) {
+    if (currentTotal != undefined) {
+      currentTotal = divide(currentTotal, currentNumber);
+      updateTopDisplay(`${currentTotal} /`);
+      updateBottomDisplay("");
+    }
+
+    if (currentTotal === undefined) {
+      currentTotal = currentNumber;
+      updateTopDisplay(`${currentTotal} /`);
+      updateBottomDisplay("");
+    }
+    // Empty array for next number
+    digitsArray = [];
+    numberBtnPressed = false;
+    decimalButton.disabled = false;
+  }
+  decimalButton.disabled = false;
 });
 
 equalsButton.addEventListener("click", function (e) {
-  operate();
-});
+  e.stopPropagation();
 
-resetButton.addEventListener("click", function (e) {
-  displayValue = 0;
-  num1array = [];
-  num2array = [];
-  number1 = undefined;
-  number2 = undefined;
-  selectedOperator = undefined;
-  answer = undefined;
-  updateDisplay();
-});
+  if (
+    (currentTotal != undefined) &
+    (selectedOperator != undefined) &
+    (digitsArray.length != 0)
+  ) {
+    equalsPressed = true;
+    let operatorSymbol = getOperatorSymbol();
 
-// Math operations
-function add() {
-  answer = number1 + number2;
-  console.log(answer);
-  num2array = [];
-  display.innerText = answer;
-}
+    // Top display
+    updateTopDisplay(`${currentTotal} ${operatorSymbol} ${currentNumber} =`);
 
-function subtract() {
-  answer = number1 - number2;
-  num2array = [];
-  display.innerText = answer;
-}
-
-function multiply() {
-  answer = number1 * number2;
-  num2array = [];
-  display.innerText = answer;
-}
-
-function divide() {
-  if (number2 === 0) return "#DIV/0!";
-  else answer = num1 / num2;
-  num2array = [];
-  display.innerText = answer;
-}
-
-function operate() {
-  if (selectedOperator === "add") {
-    add();
-  } else if (selectedOperator === "subtract") {
-    subtract();
-  } else if (selectedOperator === "multiply") {
-    multiply();
-  } else if (selectedOperator === "divide") {
-    multiply();
+    // Botttom display
+    if (selectedOperator === "add") {
+      let answer = add(currentTotal, currentNumber);
+      updateBottomDisplay(`${answer}`);
+    }
+    if (selectedOperator === "subtract") {
+      let answer = subtract(currentTotal, currentNumber);
+      updateBottomDisplay(`${answer}`);
+    }
+    if (selectedOperator === "multiply") {
+      let answer = multiply(currentTotal, currentNumber);
+      updateBottomDisplay(`${answer}`);
+    }
+    if (selectedOperator === "divide") {
+      let answer = divide(currentTotal, currentNumber);
+      if (answer === "#DIV/0!") {
+        decimalButton.disabled = true;
+      }
+      updateBottomDisplay(`${answer}`);
+    }
+    // Empty array for next number
+    digitsArray = [];
+    numberBtnPressed = false;
   }
-}
+});
 
-// function operate() {
-//   console.log("new operate");
-// }
+decimalButton.addEventListener("click", function (e) {
+  e.stopPropagation();
+});
+
+resetButton.addEventListener("click", function () {
+  reset();
+});
+
+signButton.addEventListener("click", function (e) {
+  // todo
+  currentNumber = bottomDisplay.textContent * -1;
+  updateBottomDisplay(currentNumber);
+});
